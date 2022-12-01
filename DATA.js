6f0b26d8640ef26083bdc25e418cdaa92a063331
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-12-01 18:41:14 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,45 @@
+export const item = {
+    "id": "f51ba5bc-222b-4631-a691-9c1604fba30c",
+    "home_feed": [
+        {
+            "id": "02dbf2c9-b1b6-4f4e-aa48-50027205c7ab",
+            "user_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/247.png",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png",
+            "user_name": "letter",
+            "post": "special",
+            "mutual_connections": "place"
+        },
+        {
+            "id": "ad752526-9ead-47f4-a1ca-840e7ff2e84f",
+            "user_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/18.png",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png",
+            "user_name": "may",
+            "post": "my",
+            "mutual_connections": "trade"
+        },
+        {
+            "id": "966a507d-11b3-4fa1-b621-74c2da40622e",
+            "user_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/204.png",
+            "user_name": "visit",
+            "post": "these",
+            "mutual_connections": "we"
+        },
+        {
+            "id": "0d2b579f-9431-4c33-bf45-01d3dacd8e58",
+            "user_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/283.png",
+            "user_name": "life",
+            "post": "kitchen",
+            "mutual_connections": "nation"
+        },
+        {
+            "id": "d457ca0b-26f4-4e4e-86fc-dad76e9951a1",
+            "user_img": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png",
+            "profile_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/119.png",
+            "user_name": "employee",
+            "post": "college",
+            "mutual_connections": "clearly"
+        }
+    ]
+}
